5f3dbbef81f8d82a7334d76873009d471c618277
elvismaicol <elvismaicol@gmail.com>
2020-06-11 22:03:32 -0300

    Project Created

---
+++ src/main/java/com/elvismc/appemc/AppemcApplication.java
@@ -0,0 +1,13 @@
+package com.elvismc.appemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppemcApplication.class, args);
+	}
+
+}
