c9a26732dc916d55ce2cf791e32dbc7a81a0633b
elvismaicol <elvismaicol@gmail.com>
2020-06-13 16:49:47 -0300

    Service layer, component registration

---
+++ src/main/java/com/elvismc/appemc/services/UserService.java
@@ -0,0 +1,26 @@
+package com.elvismc.appemc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.elvismc.appemc.entities.User;
+import com.elvismc.appemc.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
