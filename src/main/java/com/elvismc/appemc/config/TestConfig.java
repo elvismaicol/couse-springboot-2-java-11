874fd327af0c18c7f60ca98cbdd13f754ed8c6bf
elvismaicol <elvismaicol@gmail.com>
2020-06-18 21:07:25 -0300

    Payment, one-to-one association

--- src/main/java/com/elvismc/appemc/config/TestConfig.java
+++ src/main/java/com/elvismc/appemc/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.elvismc.appemc.entities.Category;
 import com.elvismc.appemc.entities.Order;
 import com.elvismc.appemc.entities.OrderItem;
+import com.elvismc.appemc.entities.Payment;
 import com.elvismc.appemc.entities.Product;
 import com.elvismc.appemc.entities.User;
 import com.elvismc.appemc.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/elvismc/appemc/entities/Order.java
+++ src/main/java/com/elvismc/appemc/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.elvismc.appemc.entities.enums.OrderStatus;
@@ -36,6 +38,10 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	//Ao mapear relação 1 para 1 com mesmo id. É obrigatório o uso do atributo CascadeType
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -85,6 +91,14 @@ public class Order implements Serializable {
 	
 	public Set<OrderItem> getItem() {
 		return items;
+	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
