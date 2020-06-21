f0108cc4fbb132b960ceb6a4ef018782426ae274
elvismaicol <elvismaicol@gmail.com>
2020-06-20 23:44:35 -0300

    Exception handling - findById

--- src/main/java/com/elvismc/appemc/services/UserService.java
+++ src/main/java/com/elvismc/appemc/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.elvismc.appemc.entities.User;
 import com.elvismc.appemc.repositories.UserRepository;
+import com.elvismc.appemc.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
